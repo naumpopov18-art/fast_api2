00512c9af46e64ac618e598892ea33aa83befffb
naumpopov18-art <naum.popov@mail.ru>
2026-06-28 06:55:25 +0500

    Update models.py

--- models.py
+++ models.py
@@ -1,18 +1,33 @@
-from sqlalchemy import Column, Integer, String, Text, DateTime, Float, ForeignKey
+from sqlalchemy import Column, Integer, String, Text, DateTime, Float, ForeignKey, Table
 from sqlalchemy.orm import relationship
 from datetime import datetime
-from database import Base
+from app.database import Base
+
+user_roles = Table(
+    "user_roles",
+    Base.metadata,
+    Column("user_id", Integer, ForeignKey("users.id")),
+    Column("role_id", Integer, ForeignKey("roles.id"))
+)
 
 class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String(100), unique=True, nullable=False, index=True)
-    password = Column(String(100), nullable=False)  # <-- пароль в открытом виде
-    group = Column(String(20), nullable=False, default="user")
+    password_hash = Column(String(255), nullable=False)
     created_at = Column(DateTime, default=datetime.utcnow)
 
+    roles = relationship("Role", secondary=user_roles, back_populates="users")
     advertisements = relationship("Advertisement", back_populates="author_user")
+
+class Role(Base):
+    __tablename__ = "roles"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(50), unique=True, nullable=False)
+    
+    users = relationship("User", secondary=user_roles, back_populates="roles")
 
 class Advertisement(Base):
     __tablename__ = "advertisements"
